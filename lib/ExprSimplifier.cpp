8396ac8e8c0c5cc4e391c1111cf02b5fceb1534b
Martin Jonas <mjonas@fbk.eu>
2020-11-15 20:54:32 +0100

    Temporarily fix non-termination caused by simplification cycles.

--- lib/ExprSimplifier.cpp
+++ lib/ExprSimplifier.cpp
@@ -10,8 +10,6 @@ using namespace z3;
 
 expr ExprSimplifier::Simplify(expr expression)
 {
-    unsigned oldHash = 0;
-
     if (DEBUG)
     {
 	std::cout << std::endl << std::endl << "input:" << std::endl;
@@ -20,14 +18,14 @@ expr ExprSimplifier::Simplify(expr expression)
 
     expression = expression.simplify();
 
-    while (oldHash != expression.hash())
+    std::set<unsigned> seen;
+    while (seen.find(expression.hash()) == seen.end())
     {
+        seen.insert(expression.hash());
 	if (expression.is_const())
 	{
 	    return expression;
 	}
-
-	oldHash = expression.hash();
 
 	clearCaches();
 
