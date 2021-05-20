8bea24e2f262af3bd230c8d82c2c9a5032222226
Matěj Pavlík <Matej.Pavlik@isvisl.cz>
2021-05-20 21:48:36 +0200

    renew antlr4 dependency

--- lib/Config.h
+++ lib/Config.h
@@ -1,37 +1,29 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 enum ApproximationMethod { NONE, VARIABLES, OPERATIONS, BOTH };
 enum InitialOrder { INTERLEAVE_ALL, HEURISTIC, SEQUENTIAL };
 enum ReorderType { NO_REORDER, WIN2, WIN2_ITE, WIN3, WIN3_ITE, SIFT, SIFT_ITE };
+enum Approximations { NO_APPROXIMATIONS, ONLY_UNDERAPPROXIMATIONS, ONLY_OVERAPPROXIMATIONS, ALL_APPROXIMATIONS};
 
 struct Config
 {
-    ApproximationMethod approximationMethod = VARIABLES;
-    bool limitBddSizes = false;
+    ApproximationMethod approximationMethod = BOTH;
+    Approximations approximations = ALL_APPROXIMATIONS;
+    int precision = 0;
 
     InitialOrder initialOrder = HEURISTIC;
-    ReorderType reorderType = NO_REORDER;
+    ReorderType reorderType = SIFT;
 
-    bool negateMul = false;
-
-    bool propagateUnconstrained = false;
-
-    // FROM MASTER:
+    bool propagateUnconstrained = true;
     bool goalUnconstrained = true;
 
     bool useDontCares = false;
 
-    bool checkModels = false;
+    bool checkModels = true;
 
     bool flipUniversalQuantifier = false;
-
-    // FROM MASTER:
     bool propagateNecessaryBits = true;
 
-    // FROM MASTER:
     bool produceModels = false;
     bool validatingSolver = false;
 };
-
-#endif
