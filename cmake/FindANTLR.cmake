b749d12238fc030b779925f7c629c1317529e615
matejpavlik <80360712+matejpavlik@users.noreply.github.com>
2022-07-01 14:42:01 +0200

    Update FindANTLR.cmake

--- cmake/FindANTLR.cmake
+++ cmake/FindANTLR.cmake
@@ -106,6 +106,7 @@ if(ANTLR_EXECUTABLE AND Java_JAVA_EXECUTABLE)
         COMMAND ${Java_JAVA_EXECUTABLE} -jar ${ANTLR_EXECUTABLE}
                 ${InputFile}
                 -o ${ANTLR_${Name}_OUTPUT_DIR}
+                -Xexact-output-dir 
                 -no-listener
                 -Dlanguage=Cpp
                 ${ANTLR_TARGET_COMPILE_FLAGS}
