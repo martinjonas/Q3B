936a81c04e14098d300c2bfed243972116f7b6cc
Martin Jonáš <martin.jonas@mail.muni.cz>
2023-11-28 10:09:39 +0100

    Factor out simplification passes.

--- lib/ExprSimplifier.h
+++ lib/ExprSimplifier.h
@@ -10,23 +10,6 @@
 
 enum Polarity { POSITIVE, NEGATIVE, BOTH_POLARITIES };
 
-
-class EqualityPropagator : public SimplificationPass
-{
-public:
-    EqualityPropagator(z3::context &ctx) : context(&ctx) { };
-    z3::expr Apply(const z3::expr&); //override;
-    void ReconstructModel(Model &model) override;
-
-private:
-    z3::context* context;
-
-    bool getSubstitutableEquality(const z3::expr&, z3::expr*, z3::expr*);
-    bool isVar(const z3::expr&) const;
-
-    std::vector<std::pair<z3::expr, z3::expr>> appliedSubstitutions;
-};
-
 class ExprSimplifier
 {
 public:
