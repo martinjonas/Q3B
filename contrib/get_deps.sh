8f6c3196a28e07de410f192078c63a579e55ccb5
Martin Jonáš <martin.jonas@mail.muni.cz>
2025-09-29 17:04:13 +0200

    chore: Update version of Z3 dependency.

--- contrib/get_deps.sh
+++ contrib/get_deps.sh
@@ -4,7 +4,7 @@ set -x
 
 pushd contrib
 
-git clone -b z3-4.11.2 https://github.com/Z3Prover/z3.git
+git clone -b z3-4.15.3 https://github.com/Z3Prover/z3.git
 pushd z3
 cmake -S . -B build -DZ3_BUILD_LIBZ3_SHARED=False -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j4
